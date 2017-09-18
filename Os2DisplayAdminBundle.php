4da40baa930e5af212c2014b6fc9f19345ecdf85
Mikkel Ricky <rimi@aarhus.dk>
2017-09-18 14:36:37 +0200

    MKBDIGI-156: os2display/admin-bundle

---
+++ Os2DisplayAdminBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Os2Display\AdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Os2DisplayAdminBundle extends Bundle
+{
+    public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
+}
